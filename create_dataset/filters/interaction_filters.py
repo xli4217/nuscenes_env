43a9fac7066d6895a17acf999546db16f1d1c755
Xiao Li <xiaoli@satori-login-002.mit.edu>
2021-08-03 23:27:45 -0400

    update

--- create_dataset/filters/interaction_filters.py
+++ create_dataset/filters/interaction_filters.py
@@ -91,10 +91,78 @@ def is_yielding(agent_trajectories: OrderedDict, map_elements: OrderedDict):
 
     return False
     
-    
-def is_overtaking(agent_trajectories: OrderedDict, map_elements: OrderedDict):
-    '''
-    returns true if traj1 is overtaking traj2
-    '''
+def yield_filter(agent_trajectories: OrderedDict, map_elements: OrderedDict, params: dict) -> list:
+    """If agent1 and agent2 start on different paths, end up in the same path, and agent2 gets first to the joint part of the path.
 
+    Parameters
+    ----------
+    agent_trajectories: OrderedDict
+        Map from agent id to the trajectory of the agent.
+    map_elements: OrderedDict
+        Map information
+    params: dict
+        Parameters
+
+    Returns
+    -------
+    token_list: list
+        A list of tokens detected ("yield").
+    """
+    if len(list(agent_trajectories.values())) < 2:
+        return []
+
+    def compute_trajectory_stats(traj, dilation):
+        """Compute trajectory stats needed for yielding"""
+        #TOt
+        positions = traj[:, :2]
+        v = positions[1:, :] - positions[:-1, :]
+        v = np.concatenate((v, v[-2:-1, :]), axis=0)
+        spd1 = (v ** 2).sum(1)
+        t = traj[:, 2]
+        sl = LineString(positions.tolist()).buffer(dilation)
+        return positions, spd1, t, sl
+
+    target_agent_id = list(agent_trajectories.keys())[0]
+    target_agent_traj = agent_trajectories[target_agent_id]
+    yielding_dilation_radius = params["yielding_dilation_radius"]
+
+    positions1, spd1, t1, sl1 = compute_trajectory_stats(target_agent_traj, yielding_dilation_radius)
+
+    tokens = []
+    other_agent_ids = [agent_i for agent_i in agent_trajectories.keys() if agent_i != target_agent_id]
+    for other_agent_id in other_agent_ids:
+        other_agent_traj = agent_trajectories[other_agent_id]
+        positions2, spd2, t2, sl2 = compute_trajectory_stats(other_agent_traj, yielding_dilation_radius)
+        intersection = sl1.intersection(sl2)
+        idxs1 = find_points_in_region(positions1, intersection)
+        idxs2 = find_points_in_region(positions2, intersection)
+        if len(idxs1) == 0 or len(idxs2) == 0:
+            continue
+
+        # The trajectories upto the intersection.
+        positions1b = positions1[: idxs1[0], :]
+        positions2b = positions2[: idxs2[0], :]
+        if positions1b.shape[0] < 2 or positions2b.shape[0] < 2:
+            continue
+
+        sl1b = LineString(positions1b[:, :2]).buffer(yielding_dilation_radius)
+        sl2b = LineString(positions2b[:, :2]).buffer(yielding_dilation_radius)
+
+        # The regions that cross eachother
+        sl1_only = (sl1b.difference(sl2)).buffer(-yielding_dilation_radius / 2.0)
+        sl2_only = (sl2b.difference(sl1)).buffer(-yielding_dilation_radius / 2.0)
+        # Skip if no overlaps in the trajectory.
+        if len(idxs1) == 0 or len(idxs2) == 0:
+            continue
+        idx1_m1 = max(0, idxs1[0] - params["yielding_dt"])
+        if (
+            sl1_only.length > params["yielding_prefix"]  # long enough prefix for trajectory 1
+            and sl2_only.length > params["yielding_prefix"]  # long enough prefix for trajectory 2
+            and t1[idxs1[0]] - t2[idxs2[0]] > params["yielding_time_gap"]  # agent 1 is before agent 2
+            and spd1[idx1_m1] - spd1[idxs1[0]] > -0.5  # non-increasing speed.
+            and Point(positions2b[0, :]).distance(sl1_only)
+            > params["yielding_initial_distance"]  # initial point for 2 is far from trajectory 1
+        ):
+            tokens.append(create_duration_label("Yield", t2[idxs2[0]], t2[idxs2[-1]], subject=other_agent_id))
+    return tokens
     
