248360454451b8b80eda664d235f896a3a928e54
xiaoli <xiaoli@mit.edu>
2021-12-15 19:07:21 -0500

    update

--- env/dataset_env.py
+++ env/dataset_env.py
@@ -101,6 +101,8 @@ class NuScenesDatasetEnv(NuScenesAgent):
         self.adapt_one_row = func
         
     def update_all_info(self):
+        if self.df_idx >= len(self.instance_sample_idx_list)-1:
+            return 
         self.sample_idx = self.instance_sample_idx_list[self.df_idx]
         self.sample_token = self.instance_sample_token_list[self.df_idx]
 
