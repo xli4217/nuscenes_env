23b19609757c0abc96ffe9c3700163442fd1ec9f
xiaoli <xiaoli@mit.edu>
2022-02-11 10:34:55 -0500

    update

--- nuscenes2bag/scp.sh
+++ nuscenes2bag/scp.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 scp -r xiaoli@txe1-login.mit.edu:/home/gridsan/xiaoli/TRI/ppc/experiments/next/CnnLstmAgn/epoch=17-step=71/scene-0061 ./data/supercloud_data/
+mkdir ./data/supercloud_data/CnnLstmAgn_eval_data
+scp -r xiaoli@txe1-login.mit.edu:/home/gridsan/xiaoli/TRI/ppc/experiments/next/CnnLstmAgn/epoch=17-step=71/eval_data ./data/supercloud_data/CnnLstmAgn_eval_data/
